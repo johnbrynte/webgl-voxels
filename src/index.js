0f50faed1ba398c9cbf9f9e8b64574ad98f45a4e
John <johntu@kth.se>
2021-04-06 09:10:18 +0200

    Shader fixes and experiments

--- src/index.js
+++ src/index.js
@@ -72,16 +72,54 @@ const bitmap = [
   0, 0, 1, 0, 0, 1, 0, 0,
   0, 0, 0, 1, 1, 0, 0, 0
 ];
+const bitmaps = [
+  [
+    0, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 1, 0, 0, 1, 0, 0,
+    0, 1, 0, 0, 0, 0, 1, 0,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    0, 1, 0, 0, 0, 0, 1, 0,
+    0, 0, 1, 0, 0, 1, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 1, 0, 0, 1, 0, 0,
+    0, 1, 0, 0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0, 0, 1, 0,
+    0, 0, 1, 0, 0, 1, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+  ]
+];
+// const bitmaps = [
+//   [
+//     1, 1, 1, 1, 1, 1, 1, 1,
+//     1, 0, 0, 0, 0, 0, 0, 1,
+//     1, 0, 0, 0, 0, 0, 0, 1,
+//     1, 0, 0, 0, 0, 0, 0, 1,
+//     1, 0, 0, 0, 0, 0, 0, 1,
+//     1, 0, 0, 0, 0, 0, 0, 1,
+//     1, 0, 0, 0, 0, 0, 0, 1,
+//     1, 1, 1, 1, 1, 1, 1, 1
+//   ]
+// ];
 
 const imageDataSize = 32 * 32 * 4;
 const imageDataRaw = new Array(imageDataSize);
 for (var i = 0; i < imageDataSize; i += 4) {
-  var j = i / 4;
-  imageDataRaw[i] = Math.floor(Math.random() * 255);
-  imageDataRaw[i + 1] = Math.floor(Math.random() * 255);
-  imageDataRaw[i + 2] = Math.floor(Math.random() * 255);
-  // imageDataRaw[i + 3] = j < bitmap.length ? 255 * bitmap[j] : 0;
-  imageDataRaw[i + 3] = 255; //Math.random() > 0.87 ? 255 : 0;
+  var j = (i / 4);
+  var bitmapIndex = Math.floor(j / (8 * 8)) % bitmaps.length;
+  var b = bitmaps[bitmapIndex];
+  j = j % (8 * 8);
+
+  imageDataRaw[i] = Math.floor(((j % 8) / 8) * 255);
+  imageDataRaw[i + 1] = Math.floor(((j / 8) / 8) * 255);
+  imageDataRaw[i + 2] = Math.floor(((j % (8 * 8)) / (8 * 8)) * 255);
+  imageDataRaw[i + 3] = 255 * b[j];
+  // imageDataRaw[i + 3] = 255; //Math.random() > 0.87 ? 255 : 0;
 }
 const imageData = new Uint8Array(imageDataRaw);
 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 32, 32, 0, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
@@ -94,6 +132,7 @@ gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 // render
 
 var cameraTranslate = [0, 0, -8];
+var cameraRotate = 0;
 
 function render() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -109,7 +148,7 @@ function render() {
 
   const fieldOfView = (45 * Math.PI) / 180; // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 1.0;
+  const zNear = 0.1;
   const zFar = 100.0;
   const projectionMatrix = mat4.create();
 
@@ -123,11 +162,11 @@ function render() {
     modelViewMatrix, // matrix to translate
     cameraTranslate
   ); // amount to translate
-  // mat4.rotateY(
-  //   modelViewMatrix,
-  //   modelViewMatrix,
-  //   Math.PI / 8
-  // );
+  mat4.rotateZ(
+    modelViewMatrix,
+    modelViewMatrix,
+    cameraRotate
+  );
 
   mat4.invert(projectionMatrix, projectionMatrix);
   mat4.invert(modelViewMatrix, modelViewMatrix);
@@ -158,9 +197,11 @@ function render() {
 window.addEventListener("keydown", function(e) {
   if (e.code === "KeyA") {
     cameraTranslate[0] -= 1;
+    // cameraRotate += 0.01;
   }
   if (e.code === "KeyD") {
     cameraTranslate[0] += 1;
+    // cameraRotate -= 0.01;
   }
   if (e.code === "KeyW") {
     cameraTranslate[1] += 1;
@@ -169,10 +210,10 @@ window.addEventListener("keydown", function(e) {
     cameraTranslate[1] -= 1;
   }
   if (e.code === "KeyQ") {
-    cameraTranslate[2] += 1;
+    cameraTranslate[2] += 0.2;
   }
   if (e.code === "KeyE") {
-    cameraTranslate[2] -= 1;
+    cameraTranslate[2] -= 0.2;
   }
   console.log(cameraTranslate);
   render();
--- src/shader.frag
+++ src/shader.frag
@@ -13,25 +13,36 @@ varying highp vec2 vTextureCoord;
 void main() {
 
   vec4 wCamera = uModelViewMatrix * vec4(0.0,0.0,0.0,1.0);
-  vec4 wPlane = uModelViewMatrix * uProjectionMatrix * vec4(vTextureCoord.xy * uScreen, 1.0, 1.0);
+  vec4 wPlane =  uProjectionMatrix * uModelViewMatrix * vec4((vTextureCoord.xy - vec2(0.5, 0.5)) * uScreen, 1.0, 1.0);
 
-  for (int i=0; i<100; ++i) {
-    float step = (100.0 - float(i)) / 10.0;
+  vec4 color = vec4(0,0,0,0);
+  for (int i=0; i<200; ++i) {
+    float step = 2.0 * (1.0-float(i) / 200.0); //(100.0 - float(i)) / 10.0;
 
     // this should affect the perspective (?) since the checking plane gets curved
-    vec4 v = step * (wCamera + wPlane);
+    vec4 v = wCamera + (wPlane-wCamera) * step;
+    // v.z = v.z * 4.0;
+    float scale = 1.0; //0.2*uScreen.x / 8.0;
+    float depth = min(1.0, 1.0 - length(v - wCamera) / 200.0);
 
-    if (v.x > 0.0 && v.x < 8.0 && v.y > 0.0 && v.y < 8.0, v.z > 0.0 && v.z < 8.0) {
-      int x = int(v.x);
-      int y = int(v.y);
-      int z = int(v.z);
-      int i = z * 64 + y * 8 + x;
-      int j = i / 32;
-      vec2 uv = vec2(float(i) - float(j) * 32.0, float(j)) / 32.0;
+    if (v.x > 0.0 && v.x < 8.0 && v.y > 0.0 && v.y < 8.0*scale, v.z > 0.0 && v.z < 8.0*scale) {
+      int z = int(v.z/(scale*8.0));
+      int x = int(v.x/(scale*8.0));
+      int y = int(v.y/(scale*8.0));
+      int j = z * 64 + y * 8 + x;
+      int k = j / 32;
+      vec2 uv = vec2(float(j) - float(k) * 32.0, float(k)) / 32.0;
 
-      gl_FragColor = texture2D(uSampler, uv);
-    } else {
-      gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+      vec4 cell = texture2D(uSampler, uv);
+      float alpha = cell.a * depth; // * (v.z / 8.0);
+
+      if (cell.a > 0.0) {
+        color = vec4(cell.rgb, alpha);
+      }
     }
+    //  else {
+    //   // gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+    // }
   }
+  gl_FragColor = color;
 }
